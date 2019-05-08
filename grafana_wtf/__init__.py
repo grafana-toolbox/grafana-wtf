931786f070ad9535e1994cba454354034c245ed3
Andreas Motl <andreas@hiveeyes.org>
2019-05-08 02:25:41 +0200

    Bump version: 0.6.1 → 0.7.0

--- grafana_wtf/__init__.py
+++ grafana_wtf/__init__.py
@@ -1,3 +1,3 @@
 """grafana-wtf: Grep through all Grafana entities"""
 __appname__ = 'grafana-wtf'
-__version__ = '0.6.1'
+__version__ = '0.7.0'
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requires = [
 extras = {'test': []}
 
 setup(name='grafana-wtf',
-      version='0.6.1',
+      version='0.7.0',
       description='Grep through all Grafana entities in the spirit of git-wtf',
       long_description=README,
       license="AGPL 3, EUPL 1.2",
