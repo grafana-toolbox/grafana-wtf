39d2668a7790a26c886907ffb14000f8497c870b
Christian Ariza <christian.ariza@gmail.com>
2020-05-29 12:06:45 +0200

    find » tabular format: Add a tabular report to the find command

---
+++ grafana_wtf/tabular_report.py
@@ -0,0 +1,36 @@
+from collections import OrderedDict
+from grafana_wtf.report import WtfReport
+from tabulate import tabulate
+
+
+class TabularReport(WtfReport):
+    def __init__(self, grafana_url, tblfmt="psql", verbose=False):
+        self.format = tblfmt
+        super().__init__(grafana_url, verbose=verbose)
+
+    def output_items(self, label, items, url_callback):
+        items_rows = [
+            {
+                "type": label,
+                "name": self.get_item_name(item),
+                **self.get_bibdata_dict(item, URL=url_callback(item)),
+            }
+            for item in items
+        ]
+        print(tabulate(items_rows,tablefmt=self.format))
+
+    def get_bibdata_dict(self, item, **kwargs):
+
+        # Sanity checks.
+        if "dashboard" not in item.data:
+            return {}
+        bibdata = OrderedDict()
+        bibdata["Title"] = item.data.dashboard.title
+        bibdata["Folder"] = item.data.meta.folderTitle
+        bibdata["UID"] = item.data.dashboard.uid
+        bibdata["Creation date"] = f"{item.data.meta.created}"
+        bibdata["created by"] = item.data.meta.createdBy
+        bibdata["last update date"] = f"{item.data.meta.updated}"
+        bibdata["updated by"] = item.data.meta.updatedBy
+        bibdata.update(kwargs)
+        return bibdata
