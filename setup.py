325fd0ae506d0e42416acce0927344948be03894
Andreas Motl <andreas.motl@panodata.org>
2023-02-08 03:13:33 +0100

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
