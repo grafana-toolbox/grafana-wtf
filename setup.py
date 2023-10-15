d8da5db168c38ecf0a97c680b79d6957a7361371
Andreas Motl <andreas.motl@panodata.org>
2023-10-15 23:18:31 +0200

    Fix version numbers

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extras = {
 
 setup(
     name="grafana-wtf",
-    version="0.15.2",
+    version="0.16.0",
     description="Grep through all Grafana entities in the spirit of git-wtf",
     long_description=README,
     license="AGPL 3, EUPL 1.2",
